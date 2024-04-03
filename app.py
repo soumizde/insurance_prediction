989123fcf20bf3f217c1f6203d88a1de8c6999d8
Soumi De <soumizde@gmail.com>
2024-04-03 18:51:16 +0530

    Major update - app and pkl

--- app.py
+++ app.py
@@ -1,33 +1,58 @@
 from flask import Flask, request, url_for, redirect, render_template
 import pickle
-
 import numpy as np
+import pandas as pd
+import warnings
+
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
-Pkl_Filename = "rf_tuned.pkl" 
-with open(Pkl_Filename, 'rb') as file:  
+# Load the model
+with open("knn_tuned.pkl", 'rb') as file:
     model = pickle.load(file)
-@app.route('/')
 
+# Load the scaler
+with open("scaler.pkl", 'rb') as file:
+    scaler = pickle.load(file)
+
+# Define the feature names
+feature_names = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+
+@app.route('/')
 def hello_world():
     return render_template('home.html')
 
 @app.route('/predict', methods=['POST','GET'])
 def predict():
-    features = [int(x) for x in request.form.values()]
+    values = list(request.form.values())
+    # age, sex, children, smoker, region should be integers
+    # bmi should be a float
 
+    features = [
+        int(values[0]),  # age
+        int(values[1]),  # sex
+        float(values[2]),  # bmi
+        int(values[3]),  # children
+        int(values[4]),  # smoker
+        int(values[5])   # region
+    ]
     print(features)
-    final = np.array(features).reshape((1,6))
-    print(final)
-    pred = model.predict(final)[0]
-    print(pred)
-
     
+    # Create a DataFrame with the input features and matching feature names
+    final_df = pd.DataFrame([features], columns=feature_names)
+    print(final_df)
+    
+    # Scale the input features
+    final_scaled = scaler.transform(final_df)
+    
+    pred = model.predict(final_scaled)[0]
+    print(pred)
     if pred < 0:
         return render_template('op.html', pred='Error calculating Amount!')
     else:
         return render_template('op.html', pred='Expected amount is {0:.3f}'.format(pred))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
